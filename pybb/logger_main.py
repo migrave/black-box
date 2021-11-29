0c0c1d28b8414995bec90b1e9a10b16956ae8041
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2021-11-29 17:44:03 +0100

    [pybb/logger_main.py] Only left the ROS logging

--- pybb/logger_main.py
+++ pybb/logger_main.py
@@ -4,12 +4,8 @@ import time
 
 from black_box.config.config_file_reader import ConfigFileReader
 from black_box.datalogger.loggers.mongodb_logger import MongoDBLogger
-from black_box.datalogger.pyre_comm.bb_pyre_comm import BlackBoxPyreCommunicator
 
 from black_box.datalogger.data_readers.rostopic_reader import ROSTopicReader
-from black_box.datalogger.data_readers.zyre_reader import ZyreReader
-from black_box.datalogger.data_readers.json_zmq_reader import JsonZmqReader
-from black_box.datalogger.data_readers.event_reader import EventReader
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -35,27 +31,11 @@ if __name__ == '__main__':
         if reader_name != 'default': # TODO:need better way to ignore default
             readers[reader_name] = None
 
-    if config_params.zyre:
-        readers['zyre'] = ZyreReader(config_params.zyre, logger)
-
-    if config_params.zmq:
-        readers['zmq'] = JsonZmqReader(config_params.zmq.url,
-                                       config_params.zmq.port,
-                                       config_params.zmq.topics,
-                                       logger)
-
     if config_params.ros:
         readers['ros'] = ROSTopicReader('rostopic_reader',
                                         config_params.ros,
                                         config_params.default.max_frequency,
                                         logger)
-
-    if config_params.event:
-        readers['event'] = EventReader(config_params.event,
-                                       config_params.default.max_frequency,
-                                       logger)
-
-    bb_pyre_comm = BlackBoxPyreCommunicator(['ROPOD'], config_params, logger)
 
     try:
 
@@ -66,22 +46,8 @@ if __name__ == '__main__':
                 # we wait for a bit, giving time the reader to initialise
                 time.sleep(0.1)
 
-        print('[{0}] Logger configured; ready to log data'.format(config_params.zyre.node_name))
-        logging = True
+        print('[{0}] Logger configured; ready to log data')
         while True:
-            if logging != bb_pyre_comm.logging:
-                if bb_pyre_comm.logging:
-                    for reader_name in readers:
-                        if readers[reader_name]:
-                            readers[reader_name].start_logging()
-                    print('[{0}] Started logging'.format(config_params.zyre.node_name))
-                else:
-                    for reader_name in readers:
-                        if readers[reader_name]:
-                            readers[reader_name].stop_logging()
-                    print('[{0}] Stopped logging'.format(config_params.zyre.node_name))
-                logging = bb_pyre_comm.logging
-
             time.sleep(0.1)
     except (KeyboardInterrupt, SystemExit):
         print('[logger_main] Interrupted. Exiting...')
@@ -90,5 +56,3 @@ if __name__ == '__main__':
                 readers[reader_name].stop_logging()
         if readers['zyre']:
             readers['zyre'].shutdown()
-
-        bb_pyre_comm.shutdown()
